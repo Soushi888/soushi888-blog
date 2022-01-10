568a66081f6321f93b8cc6acace4d972eaa3f55e
soushi888 <sacha.pignot@protonmail.com>
2022-01-10 02:44:27 -0500

    custom html elements + read-more link + hover effect on image

--- src/components/BlogGrid.svelte
+++ src/components/BlogGrid.svelte
@@ -7,47 +7,67 @@
 	});
 </script>
 
-<div class='BlogGrid'>
+<blog-grid>
 	{#each $PostStore as post}
-		<div class='blog-card'>
+		<blog-card>
 			<h3>{post.name}</h3>
-			<div>
-				<img src={post.thumbnail} alt={post.name}>
-			</div>
-			<p>{post.excerpt}</p>
-		</div>
+			<thumbnail>
+				<a href='#articles'><img src={post.thumbnail} alt={post.name}></a>
+			</thumbnail>
+			<excerpt>
+				<p>{post.excerpt}</p>
+				<a class='read-more' href='#articles'>Lire plus...</a>
+			</excerpt>
+		</blog-card>
 	{/each}
-</div>
+</blog-grid>
 
 
 <style lang='scss'>
-  .BlogGrid {
+  blog-grid {
     display: grid;
     grid-template-columns:  1fr 1fr 1fr;
     gap: var(--space);
 
-
-    .blog-card {
+    blog-card {
       border: var(--background-dark) solid 1px;
       border-radius: 2%;
-			padding: 15px;
+      padding: 15px var(--space);
 
       h3 {
         text-align: center;
         font-size: 1.4rem;
-				font-weight: 600;
-        margin-bottom: var(--space);
+        font-weight: 600;
       }
 
-      div {
+      thumbnail {
         display: flex;
         justify-content: center;
 
         img {
           box-sizing: border-box;
-          width: 400px;
-          margin: auto;
-					border: var(--background-dark) 2px solid;
+          width: 100%;
+          margin: var(--space) auto;
+          border: var(--background-dark) 2px solid;
+          cursor: pointer;
+          transition: all 500ms ease;
+
+          &:hover {
+            filter: grayscale(0.3);
+          }
+        }
+      }
+
+      excerpt {
+        .read-more {
+					display: block;
+          text-decoration: underline;
+          margin-top: 10px;
+          cursor: pointer;
+
+					&:hover {
+						color: var(--red);
+					}
         }
       }
     }
