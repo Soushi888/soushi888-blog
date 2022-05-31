7eaaa3df0e9e94ad118a3eb25dc2dad21018b5d7
soushi888 <sacha.pignot@protonmail.com>
2022-05-31 17:56:10 -0400

    improve article content style + rename content to showdown-content + change onMount() for afterUpdate()

--- src/components/blog/BlogArticle.svelte
+++ src/components/blog/BlogArticle.svelte
@@ -1,6 +1,7 @@
 <script lang='ts'>
 	import { Converter } from 'showdown';
-	import { onMount } from 'svelte';
+	import Autolinker from 'autolinker';
+	import { afterUpdate } from 'svelte';
 	import { Discussion } from '@hiveio/dhive';
 
 	export let post: Discussion;
@@ -9,7 +10,7 @@
 	let content: HTMLDivElement;
 	let formatedDate;
 
-	onMount(async () => {
+	afterUpdate(async () => {
 		let createdDate = new Date(post.created);
 		formatedDate = `${createdDate.getDate()}-${createdDate.getMonth() + 1}-${createdDate.getFullYear()}`;
 
@@ -17,12 +18,14 @@
 		header.style.backgroundImage = `url(${image})`;
 
 		let converter = new Converter();
-		content.innerHTML = converter.makeHtml(post.body);
+		let autolinker = new Autolinker();
+		let html = converter.makeHtml(post.body);
+		content.innerHTML = autolinker.link(html);
 	});
 </script>
 
 <svelte:head>
-	<link rel='stylesheet' href='/src/styles/content.css'>
+	<link rel='stylesheet' href='/src/styles/showdown-content.css'>
 </svelte:head>
 
 <article>
@@ -32,13 +35,15 @@
 			<h2>Publié le <br>{formatedDate}</h2>
 		</div>
 	</header>
-	<div class='content' bind:this={content}></div>
+	<div class='showdown-content' bind:this={content}></div>
 </article>
 
 
 <!-- CSS -->
 <style lang='scss'>
   article {
+    background: var(--background-yellow);
+
     header {
       height: 400px;
       display: flex;
@@ -46,7 +51,7 @@
       align-items: center;
       background-size: cover;
       background-position: center;
-			background-repeat: no-repeat;
+      background-repeat: no-repeat;
       border-bottom: 2px var(--background-dark) solid;
 
       .texts {
