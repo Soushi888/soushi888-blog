6fccea1c4ba8ec70c4bed7a0a7370fe7d27e22fe
soushi888 <sacha.pignot@protonmail.com>
2022-07-08 01:40:22 -0400

    change adapter to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,35 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 import * as path from "path";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-  preprocess: preprocess({sourceMap: true}),
+	preprocess: preprocess({sourceMap: true}),
 
-  kit: {
-    adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          $models: path.resolve("/src/models"),
-          $components: path.resolve("/src/components"),
-          $stores: path.resolve("/src/stores")
-        }
-      },
-      server: {
-        fs: {
-          allow: [".."]
-        }
-      }
-    }
-  }
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		vite: {
+			resolve: {
+				alias: {
+					$models: path.resolve("/src/models"),
+					$components: path.resolve("/src/components"),
+					$stores: path.resolve("/src/stores")
+				}
+			},
+			server: {
+				fs: {
+					allow: [".."]
+				}
+			}
+		}
+	}
 };
 
 export default config;
