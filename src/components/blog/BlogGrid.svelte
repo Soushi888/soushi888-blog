ba46d2615655cdb67e7959bf34f6b1ed1be759e0
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-10-26 23:59:11 -0400

    improve style

--- src/components/blog/BlogGrid.svelte
+++ src/components/blog/BlogGrid.svelte
@@ -1,6 +1,6 @@
 <script lang='ts'>
-	import { onMount } from 'svelte';
-	import { posts, getPosts } from '$stores/hive.store';
+	import {onMount} from 'svelte';
+	import {posts, getPosts} from '$stores/hive.store';
 
 	onMount(async () => {
 		await getPosts();
@@ -13,8 +13,9 @@
 			<h3>{post.title}</h3>
 			{#if post.json_metadata.image[0]}
 				<div class='thumbnail'>
-					<a href={`/blog/${post.permlink}`}><img src={post.json_metadata.image[0]}
-																																	 alt={post.name}></a>
+					<a href={`/blog/${post.permlink}`}>
+						<img src={post.json_metadata.image[0]} alt={post.name}>
+					</a>
 				</div>
 			{/if}
 			<div class='excerpt'>
@@ -27,63 +28,59 @@
 
 
 <style lang='scss'>
-  .blog-grid {
-    display: grid;
-    grid-template-columns:  1fr 1fr 1fr;
-    gap: var(--space);
+	.blog-grid {
+		display: flex;
+		justify-content: center;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 20px;
 
-    @media screen and (max-width: 1250px) {
-      grid-template-columns:  1fr 1fr;
-    }
 
-    @media screen and (max-width: 950px) {
-      grid-template-columns:  1fr;
-    }
+		.blog-card {
+			height: max-content;
+			width: 300px;
+			align-self: center;
+			border: var(--background-dark) solid 1px;
+			border-radius: 2%;
+			padding: 15px var(--space);
 
-    .blog-card {
-      height: max-content;
-      align-self: center;
-      border: var(--background-dark) solid 1px;
-      border-radius: 2%;
-      padding: 15px var(--space);
+			h3 {
+				text-align: center;
+				font-size: 1.4rem;
+				font-weight: 600;
+			}
 
-      h3 {
-        text-align: center;
-        font-size: 1.4rem;
-        font-weight: 600;
-      }
+			.thumbnail {
+				display: flex;
+				justify-content: center;
 
-      .thumbnail {
-        display: flex;
-        justify-content: center;
+				img {
+					box-sizing: border-box;
+					width: 300px;
+					margin: var(--space) auto;
+					border: var(--background-dark) 2px solid;
+					transition: all 500ms ease;
 
-        img {
-          box-sizing: border-box;
-          width: 300px;
-          margin: var(--space) auto;
-          border: var(--background-dark) 2px solid;
-          transition: all 500ms ease;
+					&:hover {
+						filter: grayscale(0.5);
+					}
+				}
+			}
 
-          &:hover {
-            filter: grayscale(0.5);
-          }
-        }
-      }
+			.excerpt {
+				word-break: break-word;
 
-      .excerpt {
-        word-break: break-word;
+				.read-more {
+					display: block;
+					text-decoration: underline;
+					margin-top: 10px;
+					cursor: pointer;
 
-        .read-more {
-          display: block;
-          text-decoration: underline;
-          margin-top: 10px;
-          cursor: pointer;
-
-          &:hover {
-            color: var(--red);
-          }
-        }
-      }
-    }
-  }
+					&:hover {
+						color: var(--red);
+					}
+				}
+			}
+		}
+	}
 </style>
